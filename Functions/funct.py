e09c93e91c7623409998f896234c583860992f6c
plecca <arnoldmuleya701@gmail.com>
2025-08-25 11:00:56 +0200

    Functions with two parameters

--- Functions/funct.py
+++ Functions/funct.py
@@ -1,14 +1,37 @@
-def generate_full_name ():
+import math
+
+
+def generate_full_name():
     first_name = 'Arnold'
     last_name = 'Muleya'
     space = ' '
     full_name = first_name + space + last_name
     return full_name
-print(generate_full_name())
+print(generate_full_name()) 
 
-def add_two_numbers ():
-    num_one = 2
-    num_two = 3
-    total = num_one + num_two
-    return total
-print(add_two_numbers())
+
+def subtract_numbers():
+    num_one = 10
+    num_two = 5
+    subtraction = num_one - num_two
+    return subtraction
+print(subtract_numbers()) 
+
+
+def greetings (name):
+    text =' welcome to me world of python ' + name
+    return text
+print (greetings('Muleya'))
+
+def add_ten(num):
+    ten = 10
+    return num + ten
+print(add_ten(90))
+
+def area_of_sircle (r):
+    PI = math.pi
+    area = PI * r ** 2
+    return area
+print(area_of_sircle(10))
+
+
