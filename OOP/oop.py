3e207b59b56ce5a132a4fa2fc8c893a1d8dc862c
plecca <arnoldmuleya701@gmail.com>
2025-09-05 11:38:25 +0200

    Object Oriented programming

--- OOP/oop.py
+++ OOP/oop.py
@@ -1,19 +1,10 @@
-class Arnold:
-     def walk(self):
-          print('ill be back...!')
-          print('i am walking...!')
-          print('hasta la vista baby...!')
-     def talk(self):
-          print('Oh check i can talk...!')
-          print('hello there...!')
-          print('get to the chopper...!')  
-     def eat(self):
-          print('i am eating...!')
-          print('i love to eat...!')
-          print('i can eat a lot...!')    
+class Dog:
+    def __init__ (self,name, age):
+        self.name = name
+        self.age = age
+        
+m = Dog('Tommy', 9)
+print(m.name)
+print(m.age) 
 
-p = Arnold()
-p.walk()
-p.talk()
-p.eat()
-print(type(p))        
+          
