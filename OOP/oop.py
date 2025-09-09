49f0a67333d54cdc525617d8154fe3b67aeaa9f7
plecca <arnoldmuleya701@gmail.com>
2025-09-09 14:23:51 +0200

    updated OOP

--- OOP/oop.py
+++ OOP/oop.py
@@ -4,8 +4,10 @@ class Dog:
         self.age = age
         
 m = Dog('Tommy', 9)
+pf = Dog('Rex', 3)
 print(m.name)
 print(m.age) 
+print(pf.name)
 
           
 class Arnold:
@@ -27,3 +29,41 @@ p.walk()
 p.talk()
 p.eat()
 print(type(p))        
+
+class Students:
+     def __init__(self, name, age, grade):
+          self.name = name
+          self.age = age
+          self.grade = grade   # 0 -100
+
+     def get_grade(self):
+          return self.grade    
+
+class Course:
+     def __init__(self, name, max_students):
+          self.name = name
+          self.max_students = max_students
+          self.students = []
+
+     def add_student(self, student):
+          if len(self.students)< self.max_students:
+               self.students.append(student)
+               return True
+          return False
+     def get_avarage_grade(self):
+          pass
+
+s1 = Students('lunza', 20, 80)  
+s2 = Students('Tiana', 10, 60) 
+s3 = Students('Granet', 14, 76) 
+s4 = Students('Tasha', 19, 90) 
+
+course = Course('math', 3)
+course.add_student(s1)
+course.add_student(s2)
+print(course.students[0].name)
+
+          
+                
+
+         
