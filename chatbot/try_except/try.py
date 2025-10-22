afda480522ef6e95d98adaf60e76f0217ba7da88
plecca <arnoldmuleya701@gmail.com>
2025-10-22 08:13:45 +0200

    Try and except

---
+++ chatbot/try_except/try.py
@@ -0,0 +1,15 @@
+try:
+    name = input('Enter your name:')
+    year_born = input('Year you born:')
+    age = 2019 - int(year_born)
+    print(f'You are {name}. And your age is {age}.')
+except TypeError:
+    print('Type error occur')
+except ValueError:
+    print('Value error occur')
+except ZeroDivisionError:
+    print('zero division error occur')
+else:
+    print('I usually run with the try block')
+finally:
+    print('I alway run.')
